5013d8edfbbe094cd505f4c25550593cd40dd5bf
Marcin Zieliński <marcin.x.zielinski@gmail.com>
2017-12-13 22:41:21 +0100

    justify/lists/dead button, style tweaks, added another row in bar

--- src/script.js
+++ src/script.js
@@ -3,11 +3,19 @@ editor.document.designMode = 'On';
 [].forEach.call(document.getElementsByTagName('a'), v => {
   v.addEventListener('click', function(e){
 
-    //Using currentTarget insead of target due to propagation
+    //Using currentTarget instead of target due to propagation
     let command = e.currentTarget.getAttribute('data-command');
 
-    editor.focus();
-    editor.document.execCommand(command, false, null);
-    editor.focus();
+    switch(command){
+      case 'src':
+        let child = e.currentTarget.childNodes[0];
+        child.innerHTML = child.textContent === 'abc' ? '&lt/&gt' : 'abc';
+        //TODO
+        break;
+      default:
+        editor.focus();
+        editor.document.execCommand(command, false, null);
+        editor.focus();
+    }
   })
 });
