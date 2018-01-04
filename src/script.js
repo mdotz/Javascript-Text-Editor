d4601b2b577466207710ae42b97a160cf92d4e87
Marcin Zieliński <marcin.x.zielinski@gmail.com>
2018-01-04 10:34:50 +0100

    refactor

--- src/script.js
+++ src/script.js
@@ -1,20 +1,20 @@
 editor.document.designMode = 'On';
-const colors = ['#000000','#c00000','#ff0000',
+const colors = ['#ffffff' ,'#d3d3d3' ,'#000000','#c00000','#ff0000',
                 '#ffc000','#ffff00','#92d050',
                 '#00b050', '#00b0f0'];
 
 const colorIcons =
-          document.getElementById('back-color-row').getElementsByTagName('i');
+          document.getElementById('color-row').getElementsByTagName('i');
 
 let foreColorState = false;
 
 ////////////////
 
-document.getElementById('font-color-fore').addEventListener('click', function(e){
+document.getElementById('fore').addEventListener('click', function(e){
   foreColorState = true;
 });
 
-document.getElementById('font-color-back').addEventListener('click', function(e){
+document.getElementById('back').addEventListener('click', function(e){
   foreColorState = false;
 });
 
@@ -29,8 +29,29 @@ for(let i = 0; i < colors.length; i++){
 }
 
 ////////////////
+let previousTarget;
 
-[].forEach.call(document.getElementsByTagName('a'), v => {
+[].forEach.call(document.getElementsByClassName('picker'), v => {
+  v.addEventListener('click', function(e) {
+    let bar = document.getElementById('bar');
+    let row = document.getElementById('color-row');
+    let edit = document.getElementById('editor');
+    let barStyle = getComputedStyle(bar);
+    let editStyle = getComputedStyle(edit);
+
+    if((previousTarget == null || previousTarget === e.currentTarget)
+        || row.classList.contains('hidden')){
+      edit.style.flex = editStyle.flexGrow == 0.85 ? 0.775 : 0.85;
+      bar.style.flex = barStyle.flexGrow == 0.15 ? 0.225 : 0.15;
+      row.classList.toggle('row');
+      row.classList.toggle('hidden');
+    }
+
+    previousTarget = e.currentTarget;
+  });
+});
+
+[].forEach.call(document.getElementsByClassName('std'), v => {
   v.addEventListener('click', function(e){
 
     //Using currentTarget instead of target due to propagation
@@ -47,19 +68,20 @@ for(let i = 0; i < colors.length; i++){
         editor.document.execCommand(command, false, 'H1');
         editor.focus();
         break;
-      case 'fontColor':
+      /*case 'fontColor':
         let bar = document.getElementById('bar');
         let row = document.getElementById('back-color-row');
         let edit = document.getElementById('editor');
         let barStyle = getComputedStyle(bar);
         let editStyle = getComputedStyle(edit);
 
-        edit.style.flex = editStyle.flexGrow == 0.85 ? 0.775 : 0.85;
-        bar.style.flex = barStyle.flexGrow == 0.15 ? 0.225 : 0.15;
-        row.classList.toggle('row');
-        row.classList.toggle('hidden');
-
-        break;
+        if(previousTarget == null || previousTarget === e.currentTarget){
+          edit.style.flex = editStyle.flexGrow == 0.85 ? 0.775 : 0.85;
+          bar.style.flex = barStyle.flexGrow == 0.15 ? 0.225 : 0.15;
+          row.classList.toggle('row');
+          row.classList.toggle('hidden');
+        }
+        break;*/
       default:
         editor.focus();
         editor.document.execCommand(command, false, null);
